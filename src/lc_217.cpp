471713c14d2af2ce29295ac9e3dcfbf070e4d9e0
zhuwei <1787973220@qq.com>
2023-02-18 21:11:21 +0800

    add note

---
+++ src/lc_217.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+class solution {
+
+public:
+    bool containsDuplicate(vector<int>& nums)
+    {
+        
+    }
+};
+
+int main()
+{
+    vector<int> nums={1, 5, 6, 10, 11};
+    solution prob;
+
+    return prob.containsDuplicate(nums);
+}
